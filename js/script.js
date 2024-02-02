f6316a768fc5d944a3a65c3ee3022d0c292df19f
Arkadiusz Torun <arek.torun12@gmail.com>
2024-02-02 11:16:25 +0100

    change on used functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-let container = document.querySelector(".js-container");
-let button = document.querySelector(".js-button");
-let themeName = document.querySelector(".js-themeName");
+{
+    const welcome = () => {
+        console.log("witam wszytskich developerów");
+    }
+    
+    const toggleBackground = () => {
+    const container = document.querySelector(".js-container");
+    const themeName = document.querySelector(".js-themeName");
 
-button.addEventListener("click", () => {
     container.classList.toggle("button__style");
     button.classList.toggle("beigeButton");
-
     themeName.innerText = button.classList.contains("beigeButton") ? "beżowe" : "białe";
-});
+    };
+
+    const init = () => {
+    const button = document.querySelector(".js-button");
+    
+    button.addEventListener("click", toggleBackground);
+    
+    welcome();
+    };
+
+    init();
+}
