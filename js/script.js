23a45d4a3fd1da7684345883487a0808a41045a9
Arkadiusz Torun <arek.torun12@gmail.com>
2024-02-04 18:49:22 +0100

    formatted code

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@
     const welcome = () => {
         console.log("witam wszytskich developerów");
     }
-    
-    const toggleBackground = () => {
-    const container = document.querySelector(".js-container");
-    const themeName = document.querySelector(".js-themeName");
 
-    container.classList.toggle("button__style");
-    button.classList.toggle("beigeButton");
-    themeName.innerText = button.classList.contains("beigeButton") ? "beżowe" : "białe";
+    const toggleBackground = () => {
+        const container = document.querySelector(".js-container");
+        const themeName = document.querySelector(".js-themeName");
+
+        container.classList.toggle("button__style");
+        button.classList.toggle("beigeButton");
+        themeName.innerText = button.classList.contains("beigeButton") ? "beżowe" : "białe";
     };
 
     const init = () => {
-    const button = document.querySelector(".js-button");
-    
-    button.addEventListener("click", toggleBackground);
-    
-    welcome();
+        const button = document.querySelector(".js-button");
+
+        button.addEventListener("click", toggleBackground);
+
+        welcome();
     };
 
     init();
